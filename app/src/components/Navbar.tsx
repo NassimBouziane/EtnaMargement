519d9fc868bb3fc5a1ecf4c294cf76555546e0c4
plassa_r <plassa_r@etna-alternance.net>
2023-02-22 14:31:36 +0100

    feat(scanner): Add scanner page

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,17 +1,20 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function Navbar() {
+  const navigation: any = useNavigation();
   return (
     <View className="flex justify-evenly gap-2 m-1 w-[20%] h-[90%] bg-[#5863F8] rounded-3xl">
-        <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
-        <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
-        <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
-        <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
-        <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
-        <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
-        <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
-        <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
+      <Button title="Go scan" onPress={() => navigation.navigate("Scanner")} />
+      <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
+      <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
+      <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
+      <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
+      <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
+      <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
+      <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
+      <View className="w-[64px] h-[64px] bg-white border-black border-2"></View>
     </View>
-  )
+  );
 }
--- app/src/pages/Scanner.tsx
+++ app/src/pages/Scanner.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Image, Pressable, Text, View } from "react-native";
+import moment from "moment";
+import "moment/locale/fr";
 
-export default function Scanner() {
+export default function Scanner({ navigation }: any) {
+  moment.locale(); // fr
+  const date = moment().format("LT");
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Scanner Screen</Text>
+    <View className="mt-16">
+      <View className="flex flex-row justify-between items-center">
+        <Image
+          source={require("../../assets/logoEtna.png")}
+          className=" ml-5 "
+          style={{ width: 96, height: 30 }}
+        />
+        <Pressable
+          className="mr-5 pl-5 pr-5 pt-2 pb-2 bg-[#5863F8] rounded-2xl"
+          onPress={() => navigation.navigate("")}
+        >
+          <Text className="text-lg text-white">Connexion</Text>
+        </Pressable>
+      </View>
+      <Text className="mt-48 text-center text-4xl">{date}</Text>
     </View>
   );
 }
