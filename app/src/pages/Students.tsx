bd8629a64ded18acda57dd0164a5a81d8b97af37
Kosenro <62178529+Kosenro@users.noreply.github.com>
2023-02-24 14:31:01 +0100

    feat(Graph) : add GraphWeek and GraphDay

--- app/src/pages/Students.tsx
+++ app/src/pages/Students.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
+import GraphDay from "../components/GraphDay";
+import GraphWeek from "../components/GrapWeek";
 
 export default function Student() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Student Screen</Text>
+      <GraphDay></GraphDay>
+      <GraphWeek></GraphWeek>
     </View>
   );
 }
