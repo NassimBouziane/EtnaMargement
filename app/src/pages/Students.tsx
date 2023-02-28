18d72f9150b7be88ccec6bbe8791cd41fdcd6339
Paul <paul@pop-os.localdomain>
2023-02-28 14:23:40 +0100

    feature(profilepage) : begin work on ticket display

--- app/src/pages/Students.tsx
+++ app/src/pages/Students.tsx
@@ -10,7 +10,7 @@ import {
   View,
   ScrollView,
 } from "react-native";
-import { getNote, getPromo } from "../../services/etna/etna.services";
+import { getNote, getPromo, getTicket } from "../../services/etna/etna.services";
 import {
   fetchUserConnected,
   getPhoto,
@@ -27,6 +27,7 @@ export default function Student() {
   const [user, setUser] = React.useState<any>("");
   const [promo, setPromo] = React.useState<any>("");
   const [qr_value, setQr_value] = React.useState<any>("");
+  const [tickets, setTickets] = React.useState<any>(null);
 
   const UserInfo = async () => {
     const token = await AsyncStorage.getItem("token");
@@ -34,13 +35,16 @@ export default function Student() {
     const user = await getUserByLogin(user_logs.login, await JSON.parse(token));
     const promo = await getPromo(await JSON.parse(token));
     const qr_value = `${user.login}|${user.id}|${promo[0].id}`;
+    const tickets = await getTicket(await JSON.parse(token));
     //const promo = await getPromo(await JSON.parse(token))
     //const lastNote = await getNote(await JSON.parse(token), user.login, promo[0].id.toString()).then((res) => res[res.length-1])
     //console.log(lastNote.activity_name)
     //console.log(lastNote.student_mark)
+    console.log(tickets)
     setUser(user);
     setPromo(promo[0]);
     setQr_value(qr_value);
+    // setTickets(tickets);
   };
 
   useEffect(() => {
@@ -67,11 +71,7 @@ export default function Student() {
           <Text className="font-medium text-[32px] mx-auto">
             Bienvenue, {user.firstname ? user.firstname : ""}
           </Text>
-          <Pressable className="bg-[#5863F8] h-[45px] w-[125px] rounded-lg active:bg-[#3940aa] mx-auto">
-            <Text className="text-white text-[15px] my-auto mx-auto ">
-              Se déconnecter
-            </Text>
-          </Pressable>
+          
         </View>
         <View className="flex h-[270px] w-[95%] mx-auto bg-[#E3E3E3] mt-[50px] rounded-lg">
           <View className="flex flex-row justify-evenly w-full h-[64px] bg-[#5863F8]">
@@ -101,19 +101,16 @@ export default function Student() {
               <Text className="text-[11px]">
                 {promo.wall_name ? promo.wall_name  : ""}
               </Text>
-              <Text className="text-[11px]">
-                {user.birthday ? user.birthday : "birthday"}
-              </Text>
+              
               
             </View>
             
           </View>
           <View className="absolute bottom-5 right-5"><QRCODE value={qr_value ? qr_value : "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"}className="absolute"/></View>
         </View>
-        <View className="flex h-[300px] w-[95%] mx-auto bg-[#E3E3E3] mt-[50px] rounded-lg">
-          <Text className="text-[32px] my-auto mx-auto">
-            Mon graphique de la semaine
-          </Text>
+        <View className="flex h-[300px] w-[95%] mx-auto mt-[50px] rounded-lg justify-center items-center">
+          <Text className="text-[px] my-auto mx-auto">Graphique de Présence</Text>
+          <GraphWeek />
         </View>
 
         <View className="flex h-[600px] w-[95%] mx-auto bg-[#E3E3E3] mt-[50px] rounded-lg">
