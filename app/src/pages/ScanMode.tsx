d0c98c0164b03e1db4974677d3f26c4e71a37012
Kosenro <toto2bret@gmail.com>
2023-03-01 15:26:58 +0100

    feat(GraphStudents) : add graphStudents

--- app/src/pages/ScanMode.tsx
+++ app/src/pages/ScanMode.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
 import Excel from "../components/Excel";
+import GraphStudent from "../components/GraphStudent";
 import Navbar from "../components/Navbar";
 
 export default function ScanMode() {
   return (
     <View className="flex flex-row h-full">
       <Navbar />
-      <Text>ScanMode Screen</Text>
-      <Excel date="2023-02-28"/>
+      <GraphStudent login='boular_t'/>
     </View>
   );
 }
