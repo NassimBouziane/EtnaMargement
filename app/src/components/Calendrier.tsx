34c6f92fc10b03e599abeeb6c8a130bf47a10786
plassa_r <plassa_r@etna-alternance.net>
2023-03-02 21:14:56 +0100

    (feat: frontend) Fix navbar

--- app/src/components/Calendrier.tsx
+++ app/src/components/Calendrier.tsx
@@ -35,55 +35,62 @@ export default function Calendrier(props : any) {
   };
 
   return (
-		<View>
-    <Calendar
-      // Initially visible month. Default = now
-      minDate={'2023-01-01'}
-      maxDate={'2026-01-01'}
-      onDayPress={day => {
-				handleDayPress(day)
-        setDayChoose(day)
-      }}
-      monthFormat={'yyyy MM'}
-      // Hide month navigation arrows. Default = false
-      hideExtraDays={true}
-      // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
-      firstDay={1}
-      showWeekNumbers={true}
-      onPressArrowLeft={subtractMonth => subtractMonth()}
-      onPressArrowRight={addMonth => addMonth()}
-      disableAllTouchEventsForDisabledDays={true}
-      enableSwipeMonths={true}
-      markingType={'period'}
-			markedDates={markedDates}
-      theme={{
-        backgroundColor: '#ffffff',
-        calendarBackground: '#ffffff',
-        textSectionTitleColor: '#b6c1cd',
-        selectedDayBackgroundColor: '#00adf5',
-        selectedDayTextColor: '#ffffff',
-        todayTextColor: '#00adf5',
-        dayTextColor: '#2d4150',
-        textDisabledColor: '#d9e1e8',
-        dotColor: '#00adf5',
-        selectedDotColor: '#ffffff',
-        arrowColor: 'orange',
-        disabledArrowColor: '#d9e1e8',
-        monthTextColor: 'blue',
-        indicatorColor: 'blue',
-        // textDayFontFamily: 'monospace',
-        // textMonthFontFamily: 'monospace',
-        // textDayHeaderFontFamily: 'monospace',
-        textDayFontWeight: '300',
-        textMonthFontWeight: 'bold',
-        textDayHeaderFontWeight: '300',
-        textDayFontSize: 16,
-        textMonthFontSize: 16,
-        textDayHeaderFontSize: 16
-      }}
-    />
-		{ dayChoose && props.component === 'Excel' && <Excel date={dayChoose.dateString} /> }
-		{ dayChoose && props.component === 'Logs' && <Text className='text-xl color-white mx-auto'> Vous avez selectionnés le jour {dayChoose.dateString} </Text>}
-		</View>
+    <View>
+      <Calendar
+        // Initially visible month. Default = now
+        minDate={"2023-01-01"}
+        maxDate={"2026-01-01"}
+        onDayPress={(day) => {
+          handleDayPress(day);
+          setDayChoose(day);
+        }}
+        monthFormat={"yyyy MM"}
+        // Hide month navigation arrows. Default = false
+        hideExtraDays={true}
+        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+        firstDay={1}
+        showWeekNumbers={true}
+        onPressArrowLeft={(subtractMonth) => subtractMonth()}
+        onPressArrowRight={(addMonth) => addMonth()}
+        disableAllTouchEventsForDisabledDays={true}
+        enableSwipeMonths={true}
+        markingType={"period"}
+        markedDates={markedDates}
+        theme={{
+          backgroundColor: "#ffffff",
+          calendarBackground: "#ffffff",
+          textSectionTitleColor: "#b6c1cd",
+          selectedDayBackgroundColor: "#00adf5",
+          selectedDayTextColor: "#ffffff",
+          todayTextColor: "#00adf5",
+          dayTextColor: "#2d4150",
+          textDisabledColor: "#d9e1e8",
+          dotColor: "#00adf5",
+          selectedDotColor: "#ffffff",
+          arrowColor: "orange",
+          disabledArrowColor: "#d9e1e8",
+          monthTextColor: "blue",
+          indicatorColor: "blue",
+          // textDayFontFamily: 'monospace',
+          // textMonthFontFamily: 'monospace',
+          // textDayHeaderFontFamily: 'monospace',
+          textDayFontWeight: "300",
+          textMonthFontWeight: "bold",
+          textDayHeaderFontWeight: "300",
+          textDayFontSize: 16,
+          textMonthFontSize: 16,
+          textDayHeaderFontSize: 16,
+        }}
+      />
+      {dayChoose && props.component === "Excel" && (
+        <Excel date={dayChoose.dateString} />
+      )}
+      {dayChoose && props.component === "Logs" && (
+        <Text className="text-xl color-white mx-auto">
+          {" "}
+          Vous avez sélectionné le jour {dayChoose.dateString}{" "}
+        </Text>
+      )}
+    </View>
   );
 };
